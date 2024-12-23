f8b384059014ed8593d42958c1b5c1aa6331c33e
davidbelfiori <davidjulian.belfiori@students.uniroma2.eu>
2024-12-23 19:06:30 +0100

    aggiunta gui loin registrazione, homeTecnico da aggiungere cose

---
+++ src/main/java/org/ing/ispw/unifix/controllergrafico/ControllerGraficoHomeTecnico.java
@@ -0,0 +1,40 @@
+package org.ing.ispw.unifix.controllergrafico;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import org.ing.ispw.unifix.Driver;
+import org.ing.ispw.unifix.controllerapplicativo.LoginController;
+import org.ing.ispw.unifix.controllerapplicativo.TecnicoController;
+import org.ing.ispw.unifix.utils.PopUp;
+
+import java.io.IOException;
+
+public class ControllerGraficoHomeTecnico {
+    @FXML
+    private Label Welcome;
+    private LoginController lc;
+    PopUp popUp = new PopUp();
+    private TecnicoController tc;
+    public ControllerGraficoHomeTecnico() {
+
+        lc = LoginController.getInstance();
+        tc = TecnicoController.getInstance();
+    }
+
+    public void initialize() {
+        Welcome.setText("Ciao " + lc.getCurrentUser().getNome()+" ecco i tuoi interventi");
+    }
+
+
+
+    @FXML
+    protected void logout(MouseEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("login.fxml"));
+        ((Node) event.getSource()).getScene().setRoot(fxmlLoader.load());
+    }
+
+}
