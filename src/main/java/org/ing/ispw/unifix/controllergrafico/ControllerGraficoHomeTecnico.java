7ed8b599a8be0862ff2e8a0bccfa1c70808fa592
davidbelfiori <davidjulian.belfiori@students.uniroma2.eu>
2024-12-30 11:22:11 +0100

    aggiunta la modifica allo stato della segnalazione in gui

--- src/main/java/org/ing/ispw/unifix/controllergrafico/ControllerGraficoHomeTecnico.java
+++ src/main/java/org/ing/ispw/unifix/controllergrafico/ControllerGraficoHomeTecnico.java
@@ -4,12 +4,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.ing.ispw.unifix.Driver;
+import org.ing.ispw.unifix.bean.SegnalazioneBean;
 import org.ing.ispw.unifix.controllerapplicativo.LoginController;
+import org.ing.ispw.unifix.controllerapplicativo.TecnicoController;
 import org.ing.ispw.unifix.controllerapplicativo.VisualizzaSegnalazioniTecnicoController;
 import org.ing.ispw.unifix.exception.NessunaSegnalazioneException;
 import org.ing.ispw.unifix.exception.NessunaSegnalazioneTecnicoException;
@@ -30,10 +35,12 @@ public class ControllerGraficoHomeTecnico {
     @FXML
     private Label welcome;
     private LoginController lc;
+    private TecnicoController tc;
     PopUp popUp = new PopUp();
     private final VisualizzaSegnalazioniTecnicoController vstc;
 
     public ControllerGraficoHomeTecnico() {
+        tc= TecnicoController.getInstance();
         lc = LoginController.getInstance();
         vstc = VisualizzaSegnalazioniTecnicoController.getInstance();
     }
@@ -71,8 +78,33 @@ public class ControllerGraficoHomeTecnico {
         hbox.setPadding(new Insets(10));
         hbox.setStyle("-fx-background-color: #EEEEEE; -fx-border-color: #CCCCCC; -fx-border-radius: 5; -fx-background-radius: 5;");
         hbox.setOnMouseClicked(event -> {
-            // Mostra i dettagli della segnalazione
-            popUp.showSuccessPopup("Dettagli segnalazione","Edificio"+segnalazione.getEdifico()+"\nAula: "+segnalazione.getAula()+"\nOggetto: "+segnalazione.getOggettoGuasto()+"\nDescrizione: "+segnalazione.getDescrizone()+"\nStato: "+segnalazione.getStato()+"\nDocente Segnalatore: "+segnalazione.getDocente().getNome()+" "+segnalazione.getDocente().getCognome());
+            Alert alert = new Alert(Alert.AlertType.NONE);
+            alert.setTitle("Dettagli Segnalazione");
+            alert.setHeaderText("Edificio: " + segnalazione.getEdifico() +
+                    "\nAula: " + segnalazione.getAula() +
+                    "\nOggetto: " + segnalazione.getOggettoGuasto() +
+                    "\nDescrizione: " + segnalazione.getDescrizone() +
+                    "\nStato: " + segnalazione.getStato() +
+                    "\nDocente: " + segnalazione.getDocente().getNome() + " " + segnalazione.getDocente().getCognome());
+
+            // Bottone "Chiudi"
+            ButtonType chiudiButton = new ButtonType("Chiudi", ButtonBar.ButtonData.CANCEL_CLOSE);
+            // Bottone "Metti in lavorazione"
+            ButtonType lavorazioneButton = new ButtonType("lavorazione", ButtonBar.ButtonData.OK_DONE);
+            alert.getButtonTypes().setAll(chiudiButton, lavorazioneButton);
+
+            alert.showAndWait().ifPresent(response -> {
+              if (response == lavorazioneButton){
+                    tc.updateSegnalazione(new SegnalazioneBean(segnalazione.getIdSegnalzione(), "In lavorazione"));
+                    segnalazioniContainer.getChildren().clear();
+                    mostraSegnalazioniTecnico();
+              } else{
+                  //bug quando selezioni in lavorazione la chiude sempre
+                  tc.updateSegnalazione(new SegnalazioneBean(segnalazione.getIdSegnalzione(), "Chiusa"));
+                  segnalazioniContainer.getChildren().clear();
+                  mostraSegnalazioniTecnico();
+              }
+            });
         });
 
         // Aggiungi le informazioni della segnalazione
