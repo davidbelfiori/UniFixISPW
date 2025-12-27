955afe5117f4caa650265cdba714dc34ab781db9
DavidBelfiori <davidjulian.belfiori@students.uniroma2.eu>
2025-12-27 12:49:41 +0100

    aggiunta persistenza Json

---
+++ src/main/java/org/ing/ispw/unifix/exception/JsonFileException.java
@@ -0,0 +1,9 @@
+package org.ing.ispw.unifix.exception;
+
+import java.io.IOException;
+
+public class JsonFileException extends RuntimeException {
+    public JsonFileException(String message) {
+        super(message);
+    }
+}
