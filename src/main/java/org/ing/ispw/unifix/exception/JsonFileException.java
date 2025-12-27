a8468d5632966828a5cdd814b15abd7c1f05ccb7
DavidBelfiori <davidjulian.belfiori@students.uniroma2.eu>
2025-12-27 14:54:50 +0100

    remove code smells

--- src/main/java/org/ing/ispw/unifix/exception/JsonFileException.java
+++ src/main/java/org/ing/ispw/unifix/exception/JsonFileException.java
@@ -1,7 +1,5 @@
 package org.ing.ispw.unifix.exception;
 
-import java.io.IOException;
-
 public class JsonFileException extends RuntimeException {
     public JsonFileException(String message) {
         super(message);
