9ca40cf2a801f416e6dcb126919ba83246a53c14
DavidBelfiori <davidjulian.belfiori@students.uniroma2.eu>
2026-07-22 13:48:50 +0200

    enhance SysAdmin CLI with dashboard statistics and segnalazioni display

--- src/main/java/org/ing/ispw/unifix/cli/SysAdminHomeCli.java
+++ src/main/java/org/ing/ispw/unifix/cli/SysAdminHomeCli.java
@@ -1,11 +1,17 @@
 package org.ing.ispw.unifix.cli;
 
 import org.ing.ispw.unifix.bean.AulaBean;
+import org.ing.ispw.unifix.bean.NotaSegnalazioneBean;
+import org.ing.ispw.unifix.bean.SegnalazioneBean;
+import org.ing.ispw.unifix.controllerapplicativo.DashboardKpiController;
 import org.ing.ispw.unifix.controllerapplicativo.GestioneAuleController;
 
+import org.ing.ispw.unifix.controllerapplicativo.GestisciSegnalazioniAdminController;
+import org.ing.ispw.unifix.controllerapplicativo.InserisciNotaSegnalazioneController;
 import org.ing.ispw.unifix.exception.AulaGiaPresenteException;
 import org.ing.ispw.unifix.exception.AuleNonTrovateException;
 import org.ing.ispw.unifix.exception.CsvInvalidException;
+import org.ing.ispw.unifix.exception.NessunaSegnalazioneException;
 import org.ing.ispw.unifix.utils.Printer;
 
 import java.io.BufferedReader;
@@ -22,10 +28,16 @@ public class SysAdminHomeCli {
     private Boolean quit;
     private final BufferedReader br;
     private final GestioneAuleController sc;
+    private final DashboardKpiController dc;
+    private final GestisciSegnalazioniAdminController gsac;
+    private final InserisciNotaSegnalazioneController insc;
     public SysAdminHomeCli() {
         quit = FALSE;
         br = new BufferedReader(new InputStreamReader(System.in));
         sc = new GestioneAuleController();
+        dc = new DashboardKpiController();
+        gsac = new GestisciSegnalazioniAdminController();
+        insc = new InserisciNotaSegnalazioneController();
     }
 
     public void adminHome() throws IOException {
@@ -37,9 +49,8 @@ public class SysAdminHomeCli {
             Printer.print("\t3) Visualizza aule inserite");
             Printer.print("\t4) Visualizza Statistiche");
             Printer.print("\t5) Visualizza Segnalazioni");
-            Printer.print("\t6) Torna al menu principale");
-            Printer.print("\t7) Log off");
-            Printer.print("\t8) Quit");
+            Printer.print("\t6) Log off");
+            Printer.print("\t7) Quit");
             Printer.print(": ");
 
             String action = br.readLine();
@@ -51,30 +62,26 @@ public class SysAdminHomeCli {
                     } catch (AulaGiaPresenteException | CsvInvalidException e) {
                         Printer.error("Errore durante l'inserimento delle aule: " + e.getMessage());
                     }
-
                     break;
                 case "2":
                     inserisciAulaSingola();
                     break;
                 case "3":
-                    try {
-                        List<AulaBean> aule = sc.visualizzaAule();
-                        for (AulaBean aula : aule) {
-                            Printer.print("Edificio: " + aula.getEdificio());
-                            Printer.print("ID Aula: " + aula.getIdAula());
-                            Printer.print("Piano: " + aula.getPiano());
-                            Printer.print("Oggetti: " + String.join(", ", aula.getOggetti()));
-                            Printer.print("-------------------------");
-                        }
-                    } catch (AuleNonTrovateException | IllegalArgumentException e) {
-                        Printer.error("Errore"+e.getMessage());
-                    }
+                    visualizzaAuleInserite();
                     break;
-                    //CASE LogOut
-                case "7":
+
+                case "4": //case viauslizza statistiche;
+                   visualizzaStatistiche();
+                    break;
+
+                case "5": //CASE Visualizza Segnalazioni
+                    visualizzaSegnalazioniENote();
+                    break;
+
+                case "6": //CASE LogOut
                     return;
-                    //CASE EXIT;
-                case "8":
+
+                case "7"://CASE EXIT;
                     quit=TRUE;
                     break;
                 default:
@@ -85,6 +92,67 @@ public class SysAdminHomeCli {
         System.exit(0);
 
 
+    }
+
+    private void visualizzaSegnalazioniENote(){
+        try {
+            List<SegnalazioneBean> segnalazioneBeanList = gsac.getAllSegnalazioni();
+            for (SegnalazioneBean segnalazioneBean : segnalazioneBeanList) {
+                List<NotaSegnalazioneBean> note = insc.getNoteForSegnalazione(segnalazioneBean.getIdSegnalazione());
+                Printer.print("----------------------------------");
+                Printer.print("Tecnico Assegnato : "+segnalazioneBean.getTecnico().getCognome()+" "+segnalazioneBean.getTecnico().getNome());
+                Printer.print("ID Segnalazione: " + segnalazioneBean.getIdSegnalazione());
+                Printer.print("Data Creazione: " + segnalazioneBean.getDataCreazione());
+                Printer.print("Edificio: "+segnalazioneBean.getEdificio());
+                Printer.print("Aula: "+segnalazioneBean.getAula());
+                Printer.print("Oggetto Guasto: " + segnalazioneBean.getOggettoGuasto());
+                Printer.print("Docente Segnalatore :"+segnalazioneBean.getUser().getCognome()+" "+segnalazioneBean.getUser().getNome());
+                Printer.print("Stato :"+segnalazioneBean.getStato());
+                Printer.print("Descrizione :"+segnalazioneBean.getDescrizione());
+                Printer.print("Note lasciate dal tecnico:");
+                Printer.print("----------------------------------");
+                if(note.isEmpty()){
+                    Printer.print("Nessuna nota presente.");
+                    Printer.print("---------------------------------");
+                }else {
+                    for (NotaSegnalazioneBean nota : note) {
+                        Printer.print("Data: " + nota.getDataCreazione());
+                        Printer.print("Testo Nota: " + nota.getTestoNota());
+                        Printer.print("---------------------------------");
+                    }
+                }
+            }
+        }catch (NessunaSegnalazioneException | IllegalArgumentException e){
+            Printer.error("Errore"+e.getMessage());
+        }
+    }
+
+    private void visualizzaStatistiche(){
+        try{
+            Printer.print("----------DASHBOARD KPI---------------");
+            Printer.print("Numero aule gestite: "+dc.visualizzaNumeroaule());
+            Printer.print("Numero edifici gestiti: "+dc.visualizzaEdificiGestiti());
+            Printer.print("Numero segnalazioni aperte: "+dc.visualizzaSegnalazioniAttiveAdmin());
+            Printer.print("Numero segnalazioni risolte: "+dc.visualizzaSegnalazioniRisolteAdmin());
+            Printer.print("-------------------------");
+        }catch (NessunaSegnalazioneException | IllegalArgumentException e){
+            Printer.error("Errore"+e.getMessage());
+        }
+    }
+
+    private void visualizzaAuleInserite(){
+        try {
+            List<AulaBean> aule = sc.visualizzaAule();
+            for (AulaBean aula : aule) {
+                Printer.print("Edificio: " + aula.getEdificio());
+                Printer.print("ID Aula: " + aula.getIdAula());
+                Printer.print("Piano: " + aula.getPiano());
+                Printer.print("Oggetti: " + String.join(", ", aula.getOggetti()));
+                Printer.print("-------------------------");
+            }
+        } catch (AuleNonTrovateException | IllegalArgumentException e) {
+            Printer.error("Errore: " + e.getMessage());
+        }
     }
 
     private void inserisciAulaSingola() throws IOException {
